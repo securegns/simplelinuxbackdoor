5060f33dcd1f7edb0a91b3ad8b9ea8c2243fa79c
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-05-31 06:16:28 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
+import socket
+import os
+ip="localhost"#Your address or this computer address
+port=8080 #Port number you want to use
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((ip,port))
+s.listen(1)
+conn,addr=s.accept()
+while True:
+    inp=input("shell:>")
+    if inp == "exit":
+        conn.send("exit")
+        conn.close()
+     
+    elif inp.startswith("run="):
+        conn.send(inp)
+        conn.recv(1024)
 
+    else:
+        pass
